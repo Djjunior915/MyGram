a7bca082b6afc1fb25dd6913673ad5d5db6c38f4
Rafael Hernandez <rhernandez7071@southwestuniversity.edu>
2021-06-03 11:06:14 -0600

    Module 2 Assignment

---
+++ MyGram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using MyGram.MyClasses;
+
+
+namespace MyGram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Posts post = new Posts();
+            post.PostId = 1;
+            post.Post = "This is a post";
+
+            Console.WriteLine("The value if my first property is: " + post.PostId);
+            Console.WriteLine("The value if my second property is: " + post.Post);
+
+        }
+
+    }
+}
